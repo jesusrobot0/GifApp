626e45ccecdaf073bf5654e71920c783b782b320
jesusrobot0 <jesusrobot9000@icloud.com>
2023-04-16 21:11:29 -0500

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+
+export function Header() {
+  return (
+    <header className='header'>
+      <div className="wrapper">
+        <h1 className='header__title'>GifApp</h1>
+      </div>
+    </header>
+  )
+}
